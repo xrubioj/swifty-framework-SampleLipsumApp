73cd95c930865c10626415bb71acc73bff14532c
Xavier Rubio Jansana <xavier@toro-intl.com>
2018-01-24 13:32:26 +0100

    Initial Commit

---
+++ SampleLipsumApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleLipsumApp
+//
+//  Created by Xavier Rubio Jansana on 24/1/18.
+//  Copyright © 2018 xrubio.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
