61b95f8db1d800cc54f3b0a1578d8da9f6ea28fa
Xavier Rubio Jansana <xavier@toro-intl.com>
2018-01-24 17:13:16 +0100

    Initial commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+
+target 'SampleLipsumApp' do
+  use_frameworks!
+
+  pod 'Lipsum', :path => '../Lipsum'
+
+  #target 'SampleLipsumAppTests' do
+  #  inherit! :search_paths
+  #end
+
+end
--- SampleLipsumApp/ViewController.swift
+++ SampleLipsumApp/ViewController.swift
@@ -1,25 +1,23 @@
-//
-//  ViewController.swift
-//  SampleLipsumApp
-//
-//  Created by Xavier Rubio Jansana on 24/1/18.
-//  Copyright © 2018 xrubio.com. All rights reserved.
-//
-
 import UIKit
+import Lipsum
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet
+    var lipsumMessage: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction
+    func getNewLipsum() {
+        lipsumMessage?.text = Lipsum.makeLipsum()
+    }
+    
 }
 
